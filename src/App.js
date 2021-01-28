83048241db340bd6bc415c2e0972c06859e248b6
Samuel Adjei <info.samadjei@gmail.com>
2021-01-28 22:50:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { IoMoonOutline } from 'react-icons/io5';
+import { IoSearchOutline } from 'react-icons/io5';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div className="container">
+				<div className="header flex justify-between p-5">
+					<div className="title">
+						<h3 className="text-xl font-semibold">Where in the world?</h3>
+					</div>
+					<div className="theme-toggle flex items-center">
+						<IoMoonOutline  />
+						<span className="ml-3">Dark Mode</span>
+					</div>
+				</div>
+				<div className="filters">
+					<div className="input">
+						<input type="text" placeholder="Search for a country" />
+						<IoSearchOutline />
+					</div>
+					<select name="region" id="region">
+						<option value="Africa">Africa</option>
+						<option value="America">America</option>
+						<option value="Asia">Asia</option>
+						<option value="Europe">Europe</option>
+						<option value="Oceania">Oceania</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
